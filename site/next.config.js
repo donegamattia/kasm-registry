139eac5e4b1d83cc8b55ac71a2c88ba44f0a3cbf
donegamattia <64248987+donegamattia@users.noreply.github.com>
2025-02-06 10:57:00 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'DonnyPr0xy registry',
+    description: 'The official store for Donnyproxy workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://registry.donega.eu/',
+    contactUrl: 'https://registry.donega.eu/support',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
